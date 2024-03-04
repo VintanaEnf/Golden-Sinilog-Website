569d6558d426d247b65206fe9c3f266692786885
Hugo Ren <azurajhonmichael33@gmail.com>
2024-03-04 14:20:58 +0800

    Removed Old Files Made By Me

--- Golden-Silog-System/blog/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse	
-from django.urls import path
-# Create your views here.
-
-def home(request):
-	return HttpResponse('<h1>Blog Home </h1>')
