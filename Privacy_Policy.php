f97ca0ec72ac533e29ab3ab89d32ff50b3e35b8e
Hugo Ren <azurajhonmichael33@gmail.com>
2024-04-23 22:31:34 +0800

    converted everything to php

---
+++ Privacy_Policy.php
@@ -0,0 +1,85 @@
+<html>
+    <head>
+        <title>Golden Sinilog Privacy Policy</title>
+        <link rel="icon" type="images/x-icon" href="./IMAGES/general/Gred.png">
+        <link rel = "stylesheet" href = "./CSS/home.css">
+        <link rel="stylesheet" type="text/css" href="./CSS/jm_css.css">
+    </head>
+    
+    <body>
+
+       <nav class = "flexnav">
+            <div style="display: flex; justify-content: start; align-items: center; color: white;">
+                <div style="margin-top: 10px;">
+                    <img src="./IMAGES/general/logo.png" alt="Logo" width="206" height="64">
+                </div>
+                <div>
+                    <img src="./IMAGES/general/WhiteVerticalLine.png" alt="Logo" width="1" height="64">
+                </div>
+
+                <div>
+                    <a style="text-decoration:none; font-family: aotf; color: white; margin: auto;margin-left: 30px;" href="index.php">Home</a>
+                </div>
+                <div>
+                    <a style="text-decoration:none; font-family: aotf; color: white;
+    margin: auto;margin-left: 15px;" href="aboutus.php">About Us</a>
+                </div>
+            </div>
+            <div style="justify-content: end; align-items: center; margin-right: 30px;">
+                <button class = "navbutton nav_loginbutton" type="submit" href="#">Login</button>
+                <button class = "navbutton nav_loginbutton" type="submit" href="#">Sign Up</button>
+                <button type="submit" href="#">Order Now</button>
+            </div>
+        </nav>
+
+
+
+        <section class="main_container white-text">
+                
+            <div class="container container-background white-text">
+                <div>
+                    <!--<img style="padding: 10px;" src="./IMAGES/aboutus/Golden-Sinilog.jpg" alt="Logo" width="412" height="256"> -->
+                    <h1 style="padding: 20px; text-align: center; font-size: 50; font-family: univers; margin-bottom:5px;">Privacy Policy</h1>
+                    <p>
+                    At Golden Sinilog Management, we are committed to protecting the privacy of our users. This Privacy Policy outlines the types of personal information we collect, how we use it, and the steps we take to ensure its security. By using our website, you consent to the data practices described in this statement.
+                    </p><p>
+                    <b style="font-size: 30;">Information We Collect:</b>
+                    </p><p>
+                    <b style="font-size: 23;">1. Personal Information:</b> We may collect personal identification information from users in a variety of ways, including but not limited to when users visit our site, register on the site, place an order, subscribe to the newsletter, respond to a survey, fill out a form, and in connection with other activities, services, features, or resources we make available on our site. 
+                    </p><p>
+                    <b style="font-size: 23;">2. Usage Information:</b> We may also collect non-personal information about how you interact with our website and services, such as your IP address, browser type, operating system, and referring URLs. This information helps us analyze trends, administer the site, track users' movements, and gather demographic information.  
+                    </p><p>
+                    <b style="font-size: 30;">How We Use Your Information:</b> 
+                    </p><p>
+                    <b style="font-size: 23;">1. To Provide Services:</b>We use your personal information to provide and maintain our services, process transactions, and communicate with you about your account and orders. 
+                    </p><p>
+                    <b style="font-size: 23;">2. To Improve Our Services:</b> We may use your information to understand how our website and services are used, identify areas for improvement, and personalize your experience.  
+                    </p><p>
+                    <b style="font-size: 23;">3. For Marketing Purposes: </b> With your consent, we may send you promotional emails about new products, special offers, or other information we think you may find interesting.   
+                    </p><p>
+                    <b style="font-size: 30;">Sharing Your Information:</b>
+                    </p><p>
+                    We may also disclose your information to comply with legal obligations, enforce our policies, respond to claims, protect the rights, property, or safety of ourselves or others, or in the event of a merger, acquisition, or sale of all or a portion of our assets. 
+                    </p><p>
+                    <b style="font-size: 30;">Security of Your Information: </b>
+                    </p><p>
+                    We take reasonable measures to protect your personal information from unauthorized access, use, alteration, or destruction. However, no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.  
+                    </p>
+                </div>
+
+                
+            
+            </div>
+        </section>
+
+        <footer class="container" style="padding: 10px 1%; color: #858585;">
+                    <p>Located at B5 L6 PH4L MAIN ROAD GOLDEN CITY STA ROSA LAGUNA.</p>
+                    <p>Copyright © 2024 Golden Sinilog, All rights reserved.</p>
+                    
+        </footer>
+        
+    </body>
+
+   
+
+</html>
---
+++ aboutus.php
@@ -0,0 +1,73 @@
+<html>
+    <head>
+        <title>Golden Sinilog About Us</title>
+        <link rel="icon" type="images/x-icon" href="./IMAGES/general/Gred.png">
+        <link rel="stylesheet" type="text/css" href="./CSS/jm_css.css">
+    </head>
+    <body>
+
+       <nav class = "flexnav">
+            <div style="display: flex; justify-content: start; align-items: center; color: white;">
+                <div style="margin-top: 10px;">
+                    <img src="./IMAGES/general/logo.png" alt="Logo" width="206" height="64">
+                </div>
+                <div>
+                    <img src="./IMAGES/general/WhiteVerticalLine.png" alt="Logo" width="1" height="64">
+                </div>
+
+                <div>
+                    <a style="text-decoration:none; font-family: aotf; color: white; margin: auto;margin-left: 30px;" href="index.php">Home</a>
+                </div>
+                <div>
+                    <a style="text-decoration:none; font-family: aotf; color: white;
+    margin: auto;margin-left: 15px;" href="aboutus.php">About Us</a>
+                </div>
+            </div>
+            <div style="justify-content: end; align-items: center; margin-right: 30px;">
+                <button class = "navbutton nav_loginbutton" type="submit" href="#">Login</button>
+                <button class = "navbutton nav_loginbutton" type="submit" href="#">Sign Up</button>
+                <button type="submit" href="#">Order Now</button>
+            </div>
+        </nav>
+
+
+
+
+
+
+        <section class="main_container white-text">
+                
+            <div class="container container-background white-text">
+                <div>
+                    <!--<img style="padding: 10px;" src="./IMAGES/aboutus/Golden-Sinilog.jpg" alt="Logo" width="412" height="256"> -->
+                    <h1 style="padding: 20px; text-align: center; font-size: 30; font-family: univers; margin-bottom:5px;">About Us</h1>
+                    <p>
+                    <B> Welcome to Golden Sinilog</B> – where tradition meets taste in a symphony of flavors that dance to the rhythm of Filipino heritage. Born from a passion for celebrating the rich culinary landscape of the Philippines, our journey is a homage to the iconic Sinigang dish and the vibrant Sinulog festival, encapsulating the warmth and festivity of Filipino culture.
+                    </p><p>
+                    At Golden Sinilog, our story began with a simple vision: to craft a dining experience that feels like a festive celebration, one that brings people together around the shared table of our nation’s beloved flavors. Nestled in the heart of our community, we embarked on this culinary adventure with a commitment to authenticity, innovation, and the highest quality of ingredients sourced directly from local farmers and suppliers.
+                    </p><p>
+                    <b>Our Mission</b> is to offer more than just a meal – we aim to provide a cultural experience that educates, inspires, and delights. Each dish we serve is a testament to our dedication to the craft of cooking and the joy of sharing good food. Through our unique blend of traditional Sinigang and the spirited energy of Sinulog, we invite our guests to embark on a flavorful journey that pays tribute to Filipino culinary excellence.
+                    </p><p>
+                    <b>Our Values</b> are rooted in the spirit of Bayanihan, the Filipino concept of communal unity and cooperation. We believe in the power of community and strive to create a space where everyone feels welcome, appreciated, and connected. Sustainability, respect for our heritage, and a constant drive for innovation guide our path forward as we continue to honor the past while embracing the future.
+                    </p><p>
+                    <b>The Team</b> behind Golden Sinilog is a diverse group of culinary enthusiasts, led by the leaders, a visionary chef with a deep love for Filipino cuisine and a creative flair for reimagining traditional recipes. Together, we are a family, united by our passion for food and our desire to make Golden Sinilog a beacon of Filipino culture and hospitality.
+                    </p><p>
+                    As we move forward, we remain dedicated to celebrating the stories and flavors that define the Filipino culinary tradition. We invite you to join us at Golden Sinilog, where every meal is a celebration, and every guest is part of our growing family.
+                    </p><p>
+                    Mabuhay, and welcome to Golden Sinilog – where every bite tells a story.</p> 
+                </div>
+
+                
+            
+            </div>
+        </section>
+
+        <footer class="container" style="padding: 10px 1%; color: #858585;">
+                    <p>Located at B5 L6 PH4L MAIN ROAD GOLDEN CITY STA ROSA LAGUNA.</p>
+                    <p>Copyright © 2024 Golden Sinilog, All rights reserved.</p>
+                    <a style="color: #858585; margin-right: 1%; font-family: aotf;" href="#">Privacy Policy</a>            
+                    <a style="color: #858585; margin-left: 1%; font-family: aotf;" href="#">Terms of Use</a>
+        </footer>
+
+    </body>
+</html>
